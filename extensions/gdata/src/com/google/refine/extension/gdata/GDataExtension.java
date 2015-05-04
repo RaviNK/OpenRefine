61162e34d09dd359a5aca17c0ad5bcf7b91d55fb
Jacky <JackyQ2015@gmail.com>
2015-05-04 11:01:18 -0400

    upgraded the google API client from 1.13.2 to 1.18. But broken the jython: 10:36:10.240 [                butterfly] > initialize modules (0ms) Exception in thread "main" java.lang.ExceptionInInitializerError 	at sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method) 	at sun.reflect.NativeMethodAccessorImpl.invoke(Unknown Source) 	at sun.reflect.DelegatingMethodAccessorImpl.invoke(Unknown Source) 	at java.lang.reflect.Method.invoke(Unknown Source) 	at org.mozilla.javascript.MemberBox.invoke(MemberBox.java:161) 	at org.mozilla.javascript.NativeJavaMethod.call(NativeJavaMethod.java:247) 	at org.mozilla.javascript.optimizer.OptRuntime.callProp0(OptRuntime.java:119) 	at org.mozilla.javascript.gen.c4._c1(file:/C:/src/jackyq/OpenRefine/extensions/jython/module/MOD-INF/controller.js:46) 	at org.mozilla.javascript.gen.c4.call(file:/C:/src/jackyq/OpenRefine/extensions/jython/module/MOD-INF/controller.js) 	at org.mozilla.javascript.ContextFactory.doTopCall(ContextFactory.java:398) 	at org.mozilla.javascript.ScriptRuntime.doTopCall(ScriptRuntime.java:3065) 	at org.mozilla.javascript.gen.c4.call(file:/C:/src/jackyq/OpenRefine/extensions/jython/module/MOD-INF/controller.js) 	at edu.mit.simile.butterfly.ButterflyModuleImpl.scriptInit(ButterflyModuleImpl.java:636) 	at edu.mit.simile.butterfly.ButterflyModuleImpl.init(ButterflyModuleImpl.java:94) 	at edu.mit.simile.butterfly.Butterfly.initializeModule(Butterfly.java:476) 	at edu.mit.simile.butterfly.Butterfly.configure(Butterfly.java:451) 	at edu.mit.simile.butterfly.Butterfly.init(Butterfly.java:308) 	at org.mortbay.jetty.servlet.ServletHolder.initServlet(ServletHolder.java:440) 	at org.mortbay.jetty.servlet.ServletHolder.doStart(ServletHolder.java:263) 	at com.google.refine.RefineServer.configure(Refine.java:294) 	at com.google.refine.RefineServer.init(Refine.java:208) 	at com.google.refine.Refine.init(Refine.java:114) 	at com.google.refine.Refine.main(Refine.java:108)

--- extensions/gdata/src/com/google/refine/extension/gdata/GDataExtension.java
+++ extensions/gdata/src/com/google/refine/extension/gdata/GDataExtension.java
@@ -42,7 +42,7 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson.JacksonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.gdata.client.docs.DocsService;
 import com.google.gdata.client.spreadsheet.SpreadsheetService;
 
